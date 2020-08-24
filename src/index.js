fa9d2554cd470568a79cb61994a12b6a83a2c4c0
roman <escaro@ukr.net>
2020-08-24 16:49:06 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*  ================== Contstants  ================  */
+
+/*  ================== Actions  ================  */
+
+/*  ================== Reducers  ================  */
+
+/*  ================== View ================  */
+const results = document.getElementById("results");
+const statusBox = document.getElementById("status-select");
+const pagination = document.getElementById("pagination");
+const gameResult = document.getElementById("game-result");
